604002ef4aa65b2c2c1d16cfb9bcaa18c3eaa0a2
manei <manei_cc@hotmail.com>
2015-01-24 17:31:33 +0000

    client API's endpoints

--- index.php
+++ index.php
@@ -4,8 +4,10 @@ require_once 'vendor/autoload.php';
 
 use Auth\ApiKeyAuthenticationServiceProvider,
     Auth\ApiKeyUserServiceProvider;
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
 
-$app = new \Silex\Application();
+$app = new Application();
 $app['debug'] = true;
 
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
@@ -14,7 +16,7 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 
 $app->register(new ApiKeyUserServiceProvider());
 $app->register(new ApiKeyAuthenticationServiceProvider(), array(
-    'security.apikey.param' => 'access_token',
+    'security.apikey.param' => 'apikey',
 ));
 $app->register(new Silex\Provider\SecurityServiceProvider(), array(
     'security.firewalls' => array(
@@ -26,11 +28,48 @@ $app->register(new Silex\Provider\SecurityServiceProvider(), array(
     )
 ));
 
-$account = $app['controllers_factory'];
-
-$account->get('/', function(){
-   return "Secured";
+$app->get('/', function(Application $app, Request $req){
+   return 'List of accounts for the current client';
 });
 
-$app->mount('/account', $account);
+$app->get('/savings', function(Application $app, Request $req){
+    return 'List of savings account for the current client';
+});
+
+$app->get('/savings/{accId}', function( Application $app, Request $req, $accId){
+    return $req->getUri();
+});
+
+$app->post('/savings/{accId}/deposit', function(Application $app, Request $req, $accId){
+    return $req->getUri();
+});
+
+$app->post('/savings/{accId}/withdraw', function(Application $app, Request $req, $accId){
+    return $req->getUri();
+});
+
+$app->get('/current', function(Application $app, Request $req){
+    return 'List of current accounts for the current client';
+});
+
+$app->get('/current/{accId}', function(Application $app, Request $req, $accId){
+    return $req->getUri();
+});
+
+$app->post('/current/{accId}/deposit', function(Application $app, Request $req, $accId){
+    return $req->getUri();
+});
+
+$app->post('/current/{accId}/withdraw', function(Application $app, Request $req, $accId){
+    return $req->getUri();
+});
+
+$app->get('/profile', function(Application $app, Request $req){
+    return 'Profile for the current client';
+});
+
+$app->put('/profile', function(Application $app, Request $req){
+    return 'Edit current client\'s profile';
+});
+
 $app->run();
